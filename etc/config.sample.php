77720d8ac240999bb4b59978011d8c3b888a83aa
Mark Kimsal <metrofindings@gmail.com>
2012-05-25 09:19:18 -0400

    Added sample config file.

---
+++ etc/config.sample.php
@@ -0,0 +1,19 @@
+<?php
+
+$serverList = array(
+	array(
+		'file'=>'server.php',
+		'name'=>'server',
+		'flags'=> array(
+		)
+	)
+);
+
+$workerList = array(
+	array(
+		'file'=>'sleep.php',
+		'name'=>'sleep_a',
+		'flags'=> array(
+		)
+	)
+);
