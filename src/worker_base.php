b282de73b5e7c4d4ada7556b442777b9e9bf222b
Mark Kimsal <metrofindings@gmail.com>
2015-03-25 12:09:02 -0400

    Refactor worker to accept React event loops.
    The constructor now accepts outside contexts and sockets to be able
    to integrate with the React PHP project's ZMQ component.
    
    This is necessary to integrate with Ratchet, which is built on React.

--- src/worker_base.php
+++ src/worker_base.php
@@ -33,16 +33,46 @@ class Zmws_Worker_Base {
 	public $listBackendSrv   = array('127.0.0.1');
 	protected $_socketCurrent = NULL;
 
-	public function __construct($backendPort='') {
+	/**
+	 * Arguments are optional BC only, use static factory
+	 */
+	public function __construct($backendPort='', $context=NULL, $backendSocket=NULL) {
 
 		$this->_cliFlags();
-		$this->context   = new ZMQContext();
+		//TODO remove this check at v3.0
+		if ($context === NULL) {
+			$this->context   = new ZMQContext();
+		} else {
+			$this->context   = $context;
+		}
+
 		if ($backendPort != '') {
 			$this->backendPort = $backendPort;
 		}
-		$this->backendSocket($this->backendPort);
+
+		//TODO remove this check at v3.0
+		if ($backendSocket === NULL) {
+			$this->backendSocket($this->backendPort);
+		} else {
+			$this->setBackendSocket($backendSocket);
+		}
+		$this->backendConnect();
 
 		$this->ready();
+	}
+
+	/**
+	 * Reads CLI flags and applies overrides.
+	 * Creates default context and socket.  
+	 */
+	public static function factory($backendPort='', $context=NULL, $backendSocket=NULL) {
+
+		if ($context === NULL) {
+			$context   = new ZMQContext();
+		}
+
+		$x = new static($backendPort, $context, $backendSocket);
+		return $x;
 	}
 
 	public function _cliFlags() {
@@ -83,14 +113,28 @@ class Zmws_Worker_Base {
 		$this->log("Worker connected as client @".date('r'), "I");
 	}
 
+	public function setBackendSocket($sock) {
+		$this->backend   = NULL;
+		$this->backend   = $sock;
+	}
+
+	/**
+	 * TODO: remove in 3.0
+	 * @DEPRECATED
+	 */
 	public function backendSocket($port) {
+		$this->setBackendSocket(new ZMQSocket($this->context, ZMQ::SOCKET_DEALER));
+	}
+
+	public function backendConnect($port=FALSE) {
+		if ($port === FALSE) {
+			$port = $this->backendPort;
+		}
+
 		if (!current($this->listBackendSrv)) {
 			reset($this->listBackendSrv);
 		}
 		$addrBackend = current($this->listBackendSrv);
-
-		$this->backend   = NULL;
-		$this->backend   = new ZMQSocket($this->context, ZMQ::SOCKET_DEALER);
 
 		//Reconnects with the same identity are ignored by ZMQ sockets.
 		//(unless router handover sockopt is used)
@@ -148,10 +192,10 @@ class Zmws_Worker_Base {
 		$poll->add($this->backend, ZMQ::POLL_IN);
 
 		$events = $poll->poll($read, $write, $this->hbInterval * 1000 );
-		//$events = $poll->poll($read, $write, 2000 );
+		//$this->log("poll done.", "D");
 
-		$this->log("poll done.", "D");
 		if($events > 0) {
+			$this->log(sprintf("Got %d read events", count($read)), 'D');
 			foreach($read as $socket) {
 				$this->_socketCurrent = $socket;
 				$zmsg = new Zmsg($socket);
@@ -163,80 +207,34 @@ class Zmws_Worker_Base {
 					//any comms with server resets HB retries
 					//but we don't want to treat this message 
 					// as a job, so continue
-					$this->hbRetries   = HEARTBEAT_MAXTRIES;
+					$this->onHeartbeat($zmsg);
 					continue;
 				}
 
-				$client_id = $zmsg->address();
-				//this is just to remove the address and null to
-				// test for sizes (params)
-				$bin_client_id = $zmsg->unwrap();
-
-				$p = (object)array();
-				//params
-				//id, null, params, body
-				if ($zmsg->parts() == 2) {
-					$param = $zmsg->unwrap();
-					if (strpos($param, 'PARAM') !== FALSE) {
-						list($k, $v) = explode(': ', $param, 2);
-						if (strpos($k, 'JSON') !== FALSE) {
-							$p = json_decode($v);
-						}
-						if (strpos($k, 'PHP') !== FALSE) {
-							$p = unserialize($v);
-						}
-					}
-					unset($v);
-				}
-
-				//got a real job, restart idle
-				$this->idleCount = 0;
-				$jobid = substr($jobid, 5);
-				$this->_jobidCurrent = $jobid;
-
-				//workers can return TRUE/FALSE, or an object
-				// with a status and a return value
-				try {
-					$answer = $this->work($jobid, $p);
-				} catch (Exception $e) {
-					$answer = new Zmws_Worker_Answer();
-					$answer->status = FALSE;
-				}
-				if (!is_object($answer)) {
-					$x = new Zmws_Worker_Answer();
-					if ($answer !== TRUE && $answer !== FALSE) {
-						//we have something that might be
-						// null or any non-boolean value
-						// let's treat it as a return value 
-						// with a passing status
-						$x->retval = $answer;
-						$x->status = TRUE;
-					} else {
-						// we only have T/F return from worker
-						// let's treat it as pass/fail status
-						$x->status = $answer;
-					}
-					$answer    = $x;
-				}
-				$this->sendAnswer($answer);
+				$this->onMessage($jobid, $zmsg);
 			}
 			//communication with server, reset HB retries
 			$this->hbRetries   = HEARTBEAT_MAXTRIES;
-
-			$this->log(sprintf ("hb up (%d).", $this->hbRetries), "D");
 		} else {
+			// This part is linked to the polling timeout of the socket.
+			// If we poll more frequently, then it seems like the server
+			// is dead.  This check should be based on timers.
+			//TODO: use 1 timer that is the total of the entirety
+			// of all heartbeat intervals that constitute a down server.
+			// Each comm with the server, reset the timer out to the
+			// total time again.  (This is instead of counting missed intervals)
 			$this->hbRetries--;
+			$this->log(sprintf ("hb down (%d).", $this->hbRetries), "D");
+
 			//no communication for HEARTBEAT_INTERVAL seconds
 			if ($this->hbRetries == 0) {
 				$this->log(sprintf ("Server Died."), "E");
 				//try the next server
 				next($this->listBackendSrv);
 				$this->backendSocket($this->backendPort);
+				$this->backendConnect($this->backendPort);
 				$this->frontendSocket($this->frontendPort);
 				$this->ready();
-			} else {
-//				printf ("hb down (%d).%s", $this->hbRetries, PHP_EOL);
-				$this->log(sprintf ("hb down (%d).", $this->hbRetries), "D");
 			}
 		}
 
@@ -300,6 +298,66 @@ class Zmws_Worker_Base {
 		return FALSE;
 	}
 
+	public function onHeartbeat($zmsg) {
+		$this->hbRetries   = HEARTBEAT_MAXTRIES;
+	}
+
+	public function onMessage($body, $zmsg) {
+		$client_id = $zmsg->address();
+		//this is just to remove the address and null to
+		// test for sizes (params)
+		$bin_client_id = $zmsg->unwrap();
+
+		$p = (object)array();
+		//params
+		//id, null, params, body
+		if ($zmsg->parts() == 2) {
+			$param = $zmsg->unwrap();
+			if (strpos($param, 'PARAM') !== FALSE) {
+				list($k, $v) = explode(': ', $param, 2);
+				if (strpos($k, 'JSON') !== FALSE) {
+					$p = json_decode($v);
+				}
+				if (strpos($k, 'PHP') !== FALSE) {
+					$p = unserialize($v);
+				}
+			}
+			unset($v);
+		}
+
+		//got a real job, restart idle
+		$this->idleCount = 0;
+
+		//remove initial JOB:_
+		$jobid = substr($body, 5);
+		$this->_jobidCurrent = $jobid;
+
+		//workers can return TRUE/FALSE, or an object
+		// with a status and a return value
+		try {
+			$answer = $this->work($jobid, $p);
+		} catch (Exception $e) {
+			$answer = new Zmws_Worker_Answer();
+			$answer->status = FALSE;
+		}
+		if (!is_object($answer)) {
+			$x = new Zmws_Worker_Answer();
+			if ($answer !== TRUE && $answer !== FALSE) {
+				//we have something that might be
+				// null or any non-boolean value
+				// let's treat it as a return value 
+				// with a passing status
+				$x->retval = $answer;
+				$x->status = TRUE;
+			} else {
+				// we only have T/F return from worker
+				// let's treat it as pass/fail status
+				$x->status = $answer;
+			}
+			$answer    = $x;
+		}
+		$this->sendAnswer($answer);
+	}
 
 	/**
 	 * Always log E
